5f536fc0c0293ee58bfbc166495fc5b9646a5419
Ankit Pal <ankitpal59911@gmail.com>
2026-09-18 23:07:25 +0530

    Add files via upload

---
+++ 04_roman_to_integer_solution.py
@@ -0,0 +1,26 @@
+#Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+#Given a roman numeral, convert it to an integer.
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                total -= values[s[i]]
+            else:
+                total += values[s[i]]
+        return total 
+
+ 
