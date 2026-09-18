d2fddbe3637c16dad2e6b6744ec94dfee4912d96
Ankit Pal <ankitpal59911@gmail.com>
2026-09-18 23:07:49 +0530

    Add files via upload

---
+++ 05_len_of_last_word_solution.py
@@ -0,0 +1,17 @@
+#Given a string s consisting of words and spaces, return the length of the last word in the string.
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        length = 0
+        i = len(s) - 1
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+        return length
+
+
