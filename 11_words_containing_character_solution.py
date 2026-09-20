fc9b721b215b3f66f2fb0dc20567cede426c8a07
Ankit Pal <ankitpal59911@gmail.com>
2026-09-20 19:44:13 +0530

    Add files via upload

---
+++ 11_words_containing_character_solution.py
@@ -0,0 +1,15 @@
+#You are given a 0-indexed array of strings words and a character x.
+#Return an array of indices representing the words that contain the character x.
+#Note that the returned array may be in any order.
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+        output = []
+        for i in range(len(words)):
+            if x in words[i]:
+                output.append(i)
+        return output
