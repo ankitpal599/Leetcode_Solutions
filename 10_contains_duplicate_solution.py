64974d416cbc6bb14c1b3c390734fc7dae12a7ca
Ankit Pal <ankitpal59911@gmail.com>
2026-09-20 19:43:50 +0530

    Add files via upload

---
+++ 10_contains_duplicate_solution.py
@@ -0,0 +1,13 @@
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        unique_number = set()
+        for n in nums:
+            if n in unique_number:
+                return True
+            unique_number.add(n)
+        return False        
