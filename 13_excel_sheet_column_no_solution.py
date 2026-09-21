b5746739a1974b82ad7208025ddd5190801265cc
Ankit Pal <ankitpal59911@gmail.com>
2026-09-21 21:13:34 +0530

    Add files via upload

---
+++ 13_excel_sheet_column_no_solution.py
@@ -0,0 +1,19 @@
+#Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+#For example:
+#A -> 1
+#B -> 2
+#C -> 3
+#Z -> 26
+#AA -> 27
+#AB -> 28 
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        result = 0
+        for letter in columnTitle:
+            value = ord(letter) - ord('A') + 1
+            result = result * 26 + value
+        return result
