6936e87fc0875ec0d8538217e6e86822532a77b7
Ankit Pal <ankitpal59911@gmail.com>
2026-09-22 22:23:21 +0530

    Add files via upload

---
+++ 14_ugly_number_solution.py
@@ -0,0 +1,15 @@
+#An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+#Given an integer n, return true if n is an ugly number.
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        for p in [2, 3, 5]:
+            while n%p == 0:
+                n //= p
+        return n == 1
+ 
