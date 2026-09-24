02831f47f1436368ad2ab1092f3fa7d2a2ec6e25
Ankit Pal <ankitpal59911@gmail.com>
2026-09-24 20:57:09 +0530

    Add files via upload

---
+++ 18_intersection_two_arrays_solution.py
@@ -0,0 +1,12 @@
+#Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        s1 = set(nums1)
+        s2 = set(nums2)
+        return list(s1.intersection(s2))
+ 
